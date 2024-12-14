857b8bb6721edb20dc33d9d85d0630fd89d037e9
Carlos <caiquefftc@gmail.com>
2024-12-13 22:35:22 -0300

    Adicionando arquivos e pastas

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -77,7 +77,7 @@ export function Layout() {
           <SidebarContent className="bg-white">
             <div className="flex justify-end p-2">
               <SidebarTrigger>
-                <X className="text-[#f9e616] hover:text-[#f9e616]/90 h-6 w-6 cursor-pointer" />
+                <X className="text-[#d0c11d] hover:text-[#d0c11d]/90 h-6 w-6 cursor-pointer" />
               </SidebarTrigger>
             </div>
             <SidebarMenu>
@@ -102,7 +102,7 @@ export function Layout() {
       )}
 
       <div className={`flex-1 flex flex-col ${state === "expanded" ? "relative after:absolute after:inset-0 after:bg-black/20 after:backdrop-blur-sm" : ""}`}>
-        <header className="bg-[#d0c11d] h-16">
+        <header className="bg-[#420030] h-16">
           <div className="h-full max-w-7xl mx-auto px-4 flex items-center">
             {!isAdminOrClientPage && (
               <SidebarTrigger>
