8d259eebd65892141fbca2e37d6b05d5ab29ba00
Carlos <caiquefftc@gmail.com>
2024-12-13 21:22:41 -0300

    Atualizações no layout do site

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -55,7 +55,7 @@ const SidebarProvider = React.forwardRef<
 >(
   (
     {
-      defaultColapsed = true,
+      defaultOpen = true,
       open: openProp,
       onOpenChange: setOpenProp,
       className,
@@ -70,7 +70,7 @@ const SidebarProvider = React.forwardRef<
 
     // This is the internal state of the sidebar.
     // We use openProp and setOpenProp for control from outside the component.
-    const [_open, _setOpen] = React.useState(defaultColapsed)
+    const [_open, _setOpen] = React.useState(defaultOpen)
     const open = openProp ?? _open
     const setOpen = React.useCallback(
       (value: boolean | ((value: boolean) => boolean)) => {
