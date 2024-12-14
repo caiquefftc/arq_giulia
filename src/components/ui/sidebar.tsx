18ec8c6749d13f37088488cb9d3387d3ca8ae264
Carlos <caiquefftc@gmail.com>
2024-12-13 23:12:33 -0300

    Adicionando arquivos e pastas

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -55,7 +55,7 @@ const SidebarProvider = React.forwardRef<
 >(
   (
     {
-      defaultOpen = false,
+      defaultOpen = true,
       open: openProp,
       onOpenChange: setOpenProp,
       className,
@@ -263,23 +263,7 @@ const SidebarTrigger = React.forwardRef<
 >(({ className, onClick, ...props }, ref) => {
   const { toggleSidebar } = useSidebar()
 
-  return (
-    <Button
-      ref={ref}
-      data-sidebar="trigger"
-      variant="ghost"
-      size="icon"
-      className={cn("h-7 w-7", className)}
-      onClick={(event) => {
-        onClick?.(event)
-        toggleSidebar()
-      }}
-      {...props}
-    >
-      <PanelLeft />
-      <span className="sr-only">Toggle Sidebar</span>
-    </Button>
-  )
+
 })
 SidebarTrigger.displayName = "SidebarTrigger"
 
